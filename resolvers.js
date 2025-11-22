105bb990edfef609984f562ba1e43f537cd9a4cd
Abhishek <abhishekpanchal8894@gmail.com>
2025-11-22 23:10:55 +0530

    First commit

---
+++ resolvers.js
@@ -0,0 +1,85 @@
+import { GraphQLError } from 'graphql';
+import { getCompany } from './db/companies.js';
+import {
+  createJob,
+  deleteJob,
+  getJob,
+  getJobs,
+  getJobsByCompanyId,
+  updateJob,
+} from './db/jobs.js';
+
+export const resolvers = {
+  Query: {
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) throw notFoundError(`No Job Found with ID: '${id}'`);
+      return job;
+    },
+    jobs: async () => {
+      const jobs = await getJobs();
+      return jobs;
+    },
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) throw notFoundError(`No Company Found with ID: '${id}'`);
+      return company;
+    },
+  },
+
+  Mutation: {
+    createJob: async (_root, { input: { title, description } }, { user }) => {
+      if (!user) throw unauthorizedError(`Missing authentication`);
+      const { companyId } = user;
+      const job = await createJob({ companyId, title, description });
+      return job;
+    },
+    updateJob: async (
+      _root,
+      { input: { id, title, description } },
+      { user }
+    ) => {
+      if (!user) throw unauthorizedError('Missing authentication');
+      const job = await updateJob({
+        id,
+        title,
+        description,
+        companyId: user.companyId,
+      });
+      if (!job) throw notFoundError(`No Job Found with ID: '${id}'`);
+      return job;
+    },
+    deleteJob: async (_root, { input: { id } }, { user }) => {
+      console.log('[deleteJob] user:', user);
+      if (!user) throw unauthorizedError(`Missing authentication`);
+      const job = await deleteJob(id, user.companyId);
+      if (!job) throw notFoundError(`No Job Found with ID: '${id}'`);
+      return job;
+    },
+  },
+
+  Job: {
+    date: (job) => toIsoDate(job.createdAt),
+    company: (job) => getCompany(job.companyId),
+  },
+
+  Company: {
+    jobs: (company) => getJobsByCompanyId(company.id),
+  },
+};
+
+const toIsoDate = (value) => {
+  return value.slice(0, 'yyyy-mm-dd'.length);
+};
+
+const notFoundError = (message) => {
+  return new GraphQLError(message, {
+    extensions: { code: 'NOT_FOUND' },
+  });
+};
+
+const unauthorizedError = (message) => {
+  return new GraphQLError(message, {
+    extensions: { code: 'UNAUTHORIZED' },
+  });
+};
---
+++ server.js
@@ -0,0 +1,38 @@
+import cors from 'cors';
+import express from 'express';
+import { readFile } from 'node:fs/promises';
+import { authMiddleware, handleLogin } from './auth.js';
+import { expressMiddleware as apolloMiddleware } from '@as-integrations/express4';
+import { ApolloServer } from '@apollo/server';
+import { resolvers } from './resolvers.js';
+import { getUser } from './db/users.js';
+
+const PORT = 9000;
+
+const app = express();
+app.use(cors(), express.json(), authMiddleware);
+
+app.post('/login', handleLogin);
+
+const typeDefs = await readFile('./schema.graphql', 'utf8');
+
+const getContext = async ({ req }) => {
+  if (req.auth) {
+    console.log('[getContext] req.auth', req.auth);
+    const user = await getUser(req.auth.sub);
+    return { user };
+  }
+  return {};
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+await apolloServer.start();
+app.use('/graphql', apolloMiddleware(apolloServer, { context: getContext }));
+
+app.listen({ port: PORT }, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
+});
